ad5c10575d6457d734c8315d9758708a21c1cc97
JonMcCarthy7 <jtmcc93@gmail.com>
2017-08-11 21:53:31 -0700

    style and add wind direction

--- script.js
+++ script.js
@@ -5,39 +5,60 @@ $(document).ready(function(){
     var lat = data.lat;
     var long = data.lon;
 
-
       var api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=a9851a64cb9ff8ebeb57d5dd6af42a87`;
 
       $.getJSON(api, function(data){
-        var weatherType =  data.weather[0].description;
+        var weatherType =  titleCase(data.weather[0].description);
         var kTemp = data.main.temp;
         var windSpeed = data.wind.speed;
         var city = data.name;
         var tempSwap = true;
+        var dir = degreesToDirection(data.wind.deg);
         var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
 
+        function titleCase(string) {
+          return string.charAt(0).toUpperCase() + string.slice(1);
+        }
+
+        function degreesToDirection(degrees){
+            var range = 360/16;
+            var low = 360 - range/2;
+            var high = (low + range) % 360;
+            var angles = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+            for( i in angles ) {
+        	if(degrees >= low && degrees < high){
+        	    console.log(angles[i]);
+        	    return angles[i];
+        	    console.log("derp");
+          }
+        	low = (low + range) % 360;
+        	high = (high + range) % 360;
+          }
+          return "N";
+        }
 
 
-        fTemp = (kTemp*(9/5)-459.67).toFixed(2);
-        var cTemp = (kTemp-273).toFixed(2);
+
+
+        fTemp = (kTemp*(9/5)-459.67).toFixed(0);
+        var cTemp = (kTemp-273).toFixed(0);
 
         $('#city').html(city);
         $('#weatherType').html(weatherType);
-        $('#fTemp').html(fTemp + " &#8457;");
+        $('#fTemp').html(fTemp + "&#8457;");
         $('#icon').attr('src', icon);
-
         $('#fTemp').click(function(){
             if(tempSwap===false){
-              $('#fTemp').html(fTemp + " &#8457;");
+              $('#fTemp').html(fTemp + "&#8457;");
               tempSwap=true;
             } else  {
-              $('#fTemp').html(cTemp + " &#8451;");
+              $('#fTemp').html(cTemp + "&#8451;");
               tempSwap=false;
             }
         });
 
         windSpeed = (2.237*(windSpeed)).toFixed(1);
-        $('#windSpeed').html(windSpeed + " mph")
+        $('#windSpeed').html(windSpeed + "mph " + dir)
       });
 
   });
