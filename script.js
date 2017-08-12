4931f5cb9c618c0e261887efa69833b7eeb0aae0
JonMcCarthy7 <jtmcc93@gmail.com>
2017-08-11 17:59:44 -0700

    add weather icon

--- script.js
+++ script.js
@@ -1,53 +1,44 @@
 $(document).ready(function(){
 
-  var long;
-  var lat;
-  var fTemp;
-  var cTemp;
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position){
-            long = position.coords.longitude
-            lat = position.coords.latitude
-            $('#data').html("lat: " + lat + "<br>long: " + long);
+  $.getJSON("http://ip-api.com/json", function(data){
+    var lat = data.lat;
+    var long = data.lon;
 
 
       var api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=a9851a64cb9ff8ebeb57d5dd6af42a87`;
 
       $.getJSON(api, function(data){
         var weatherType =  data.weather[0].description;
-         kTemp = data.main.temp;
+        var kTemp = data.main.temp;
         var windSpeed = data.wind.speed;
         var city = data.name;
         var tempSwap = true;
+        var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+
+
 
         fTemp = (kTemp*(9/5)-459.67).toFixed(2);
-        cTemp = (kTemp-273).toFixed(2);
+        var cTemp = (kTemp-273).toFixed(2);
 
-        $('#city').html(city)
-        $('#weatherType').html(weatherType)
-        $('#fTemp').html(fTemp)
+        $('#city').html(city);
+        $('#weatherType').html(weatherType);
+        $('#fTemp').html(fTemp + " &#8457;");
+        $('#icon').attr('src', icon);
 
         $('#fTemp').click(function(){
             if(tempSwap===false){
-              $('#fTemp').html(cTemp);
+              $('#fTemp').html(fTemp + " &#8457;");
               tempSwap=true;
             } else  {
-              $('#fTemp').html(fTemp);
+              $('#fTemp').html(cTemp + " &#8451;");
               tempSwap=false;
             }
         });
 
-
-
-
-
-
-
-        $('#windSpeed').html(windSpeed)
+        windSpeed = (2.237*(windSpeed)).toFixed(1);
+        $('#windSpeed').html(windSpeed + " mph")
       });
-    });
-  }
 
-
+  });
 });
